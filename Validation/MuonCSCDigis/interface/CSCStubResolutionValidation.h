8bd04d7dce0d552f49701c80d62f7624cc351a92
Sven Dildick <sven.dildick@cern.ch>
2021-09-16 20:40:10 -0500

    Compiling version

--- Validation/MuonCSCDigis/interface/CSCStubResolutionValidation.h
+++ Validation/MuonCSCDigis/interface/CSCStubResolutionValidation.h
@@ -1,26 +1,11 @@
 #ifndef Validation_MuonCSCDigis_CSCStubResolutionValidation_H
 #define Validation_MuonCSCDigis_CSCStubResolutionValidation_H
 
-#include "FWCore/Framework/interface/ConsumesCollector.h"
-
-#include "DQMServices/Core/interface/DQMStore.h"
-#include "DataFormats/CSCDigi/interface/CSCALCTDigi.h"
-#include "DataFormats/CSCDigi/interface/CSCALCTDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCCLCTDigi.h"
-#include "DataFormats/CSCDigi/interface/CSCCLCTDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h"
-#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
 #include "Validation/MuonCSCDigis/interface/CSCBaseValidation.h"
-#include "Validation/MuonCSCDigis/interface/CSCStubMatcher.h"
 
-
-#include "Validation/MuonCSCDigis/interface/CSCBaseValidation.h"
-#include "L1Trigger/CSCTriggerPrimitives/interface/CSCLUTReader.h"
-#include "L1Trigger/CSCTriggerPrimitives/interface/ComparatorCodeLUT.h"
-
-#include <map>
 #include <string>
-#include <tuple>
+
+class CSCStubMatcher;
 
 class CSCStubResolutionValidation : public CSCBaseValidation {
 public:
@@ -30,18 +15,10 @@ public:
   void bookHistograms(DQMStore::IBooker &);
   void analyze(const edm::Event &, const edm::EventSetup &) override;
 
-  // access to the matcher
-  std::shared_ptr<CSCStubMatcher> cscStubMatcher() { return cscStubMatcher_; }
-  void setCSCStubMatcher(std::shared_ptr<CSCStubMatcher> s) { cscStubMatcher_ = s; }
-
-  
 private:
-  bool isSimTrackGood(const SimTrack &t);
-
   edm::InputTag inputTag_;
 
-  std::shared_ptr<CSCStubMatcher> cscStubMatcher_;
-  
+  std::unique_ptr<CSCStubMatcher> cscStubMatcher_;
 
   // resolution for each CSC TP; 10 CSC stations;
   MonitorElement *posresCLCT_hs[10];
@@ -52,12 +29,10 @@ private:
 
   edm::EDGetTokenT<edm::SimVertexContainer> simVertexInput_;
   edm::EDGetTokenT<edm::SimTrackContainer> simTrackInput_;
-  edm::EDGetTokenT<CSCCLCTDigiCollection> clcts_Token_;
 
   double simTrackMinPt_;
   double simTrackMinEta_;
   double simTrackMaxEta_;
-
 };
 
 #endif
--- Validation/MuonCSCDigis/src/CSCStubResolutionValidation.cc
+++ Validation/MuonCSCDigis/src/CSCStubResolutionValidation.cc
@@ -1,15 +1,5 @@
-#include "DataFormats/Common/interface/Handle.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "Validation/MuonCSCDigis/interface/CSCStubResolutionValidation.h"
-
-#include "DQMServices/Core/interface/DQMStore.h"
-#include "Geometry/CSCGeometry/interface/CSCGeometry.h"
-#include "Geometry/CSCGeometry/interface/CSCLayerGeometry.h"
-#include "DataFormats/CSCDigi/interface/CSCCLCTDigi.h"
-
-#include "Validation/MuonHits/interface/CSCSimHitMatcher.h"
-#include "DataFormats/CSCDigi/interface/CSCConstants.h"
-
+#include "Validation/MuonCSCDigis/interface/CSCStubMatcher.h"
 
 CSCStubResolutionValidation::CSCStubResolutionValidation(const edm::ParameterSet& pset, edm::ConsumesCollector&& iC)
     : CSCBaseValidation(pset) {
@@ -17,18 +7,9 @@ CSCStubResolutionValidation::CSCStubResolutionValidation(const edm::ParameterSet
   simVertexInput_ = iC.consumes<edm::SimVertexContainer>(simVertex.getParameter<edm::InputTag>("inputTag"));
   const auto& simTrack = pset.getParameter<edm::ParameterSet>("simTrack");
   simTrackInput_ = iC.consumes<edm::SimTrackContainer>(simTrack.getParameter<edm::InputTag>("inputTag"));
-  simTrackMinPt_ = simTrack.getParameter<double>("minPt");
-  simTrackMinEta_ = simTrack.getParameter<double>("minEta");
-  simTrackMaxEta_ = simTrack.getParameter<double>("maxEta");
 
-  // all CSC TPs have the same label
-  const auto& stubConfig = pset.getParameterSet("cscALCT");
-  inputTag_ = stubConfig.getParameter<edm::InputTag>("inputTag");
-  clcts_Token_ = iC.consumes<CSCCLCTDigiCollection>(inputTag_);
-  
   // Initialize stub matcher
   cscStubMatcher_.reset(new CSCStubMatcher(pset, std::move(iC)));
-
 }
 
 CSCStubResolutionValidation::~CSCStubResolutionValidation() {}
@@ -36,7 +17,7 @@ CSCStubResolutionValidation::~CSCStubResolutionValidation() {}
 //create folder for resolution histograms and book them
 void CSCStubResolutionValidation::bookHistograms(DQMStore::IBooker& iBooker) {
   iBooker.setCurrentFolder("MuonCSCDigisV/CSCDigiTask/Stub/Resolution/");
-  
+
   for (int i = 1; i <= 10; ++i) {
     int j = i - 1;
     const std::string cn(CSCDetId::chamberName(i));
@@ -52,8 +33,8 @@ void CSCStubResolutionValidation::bookHistograms(DQMStore::IBooker& iBooker) {
 
     //Slope resolution; CLCT
     std::string t4 = "CLCTBendRes_" + cn;
-   
-    bendresCLCT[j] = iBooker.book1D(t4, cn + " CLCT Bend Resolution; Slope_{L1T} - Slope_{SIM}; Entries", 50, -0.5, 0.5);  
+
+    bendresCLCT[j] = iBooker.book1D(t4, cn + " CLCT Bend Resolution; Slope_{L1T} - Slope_{SIM}; Entries", 50, -0.5, 0.5);
   }
 }
 
@@ -62,24 +43,17 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
   // Define handles
   edm::Handle<edm::SimTrackContainer> sim_tracks;
   edm::Handle<edm::SimVertexContainer> sim_vertices;
-  edm::Handle<CSCCLCTDigiCollection> clcts;
-  
+
   // Use token to retreive event information
   e.getByToken(simTrackInput_, sim_tracks);
   e.getByToken(simVertexInput_, sim_vertices);
-  e.getByToken(clcts_Token_, clcts);
-  
+
   // Initialize StubMatcher
   cscStubMatcher_->init(e, eventSetup);
 
-  
   const edm::SimTrackContainer& sim_track = *sim_tracks.product();
   const edm::SimVertexContainer& sim_vert = *sim_vertices.product();
-  
-  if (!clcts.isValid()) {
-    edm::LogError("CSCStubResolutionValidation") << "Cannot get CLCTs by label " << inputTag_.encode();
-  }
-  
+
   // select simtracks for true muons
   edm::SimTrackContainer sim_track_selected;
   for (const auto& t : sim_track) {
@@ -95,7 +69,7 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
   // Loop through good tracks, use corresponding vertex to match stubs, then fill hists of chambers where the stub appears.
   for (const auto& t : sim_track_selected) {
     std::vector<bool> hitCLCT(10);
-    
+
     std::vector<float> delta_fhs_clct(10);
     std::vector<float> delta_fqs_clct(10);
     std::vector<float> delta_fes_clct(10);
@@ -108,7 +82,7 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
     // Store matched stubs.
     // Key: ChamberID, Value : CSCStubDigiContainer
     const auto& clcts = cscStubMatcher_->clcts();
-    
+
 
     //CLCTs
     for (auto& [id, container] : clcts) {
@@ -118,7 +92,7 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
       //get the best clct in chamber
       const auto& clct = cscStubMatcher_->bestClctInChamber(id);
       if (!clct.isValid()) continue;
-      
+
       hitCLCT[chamberType - 1] = true;
 
       //calculate deltastrip
@@ -126,7 +100,7 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
       if (cscId.station() == 1 and cscId.ring() == 4 and clct.getKeyStrip() > CSCConstants::MAX_HALF_STRIP_ME1B)
       	deltaStrip = CSCConstants::NUM_HALF_STRIPS_ME1B;
 
-      //get the matched stub's keystrip 
+      //get the matched stub's keystrip
       // const int hs_clct = clct.getKeyStrip(2);
       // const int qs_clct = clct.getKeyStrip(4);
       // const int es_clct = clct.getKeyStrip(8);
@@ -139,7 +113,7 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
       // in half-strips per layer
       const float slopeHalfStrip(clct.getFractionalSlope());
       const float slopeStrip(slopeHalfStrip / 2.);
-	
+
       //get the fit hits in chamber for true value
       float stripIntercept, stripSlope;
       cscStubMatcher_->cscDigiMatcher()->muonSimHitMatcher()->fitHitsInChamber(id, stripIntercept, stripSlope);
@@ -152,7 +126,7 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
 
       const float strip_csc_sh = stripIntercept;
       const float bend_csc_sh = stripSlope;
-      
+
       delta_fhs_clct[chamberType - 1] = fhs_clct - deltaStrip - strip_csc_sh;
       delta_fqs_clct[chamberType - 1] = fqs_clct - deltaStrip - strip_csc_sh;
       delta_fes_clct[chamberType - 1] = fes_clct - deltaStrip - strip_csc_sh;
@@ -176,30 +150,9 @@ void CSCStubResolutionValidation::analyze(const edm::Event& e, const edm::EventS
 	posresCLCT_hs[i]->Fill(delta_fhs_clct[i]);
 	posresCLCT_qs[i]->Fill(delta_fqs_clct[i]);
 	posresCLCT_es[i]->Fill(delta_fes_clct[i]);
-	
+
 	bendresCLCT[i]->Fill(dslope_clct[i]);
       }
     }
   }
 }
-
-bool CSCStubResolutionValidation::isSimTrackGood(const SimTrack& t) {
-  // SimTrack selection
-  if (t.noVertex())
-    return false;
-  if (t.noGenpart())
-    return false;
-  // only muons
-  if (std::abs(t.type()) != 13)
-    return false;
-  // pt selection
-  if (t.momentum().pt() < simTrackMinPt_)
-    return false;
-  // eta selection
-  const float eta(std::abs(t.momentum().eta()));
-  if (eta > simTrackMaxEta_ || eta < simTrackMinEta_)
-    return false;
-  return true;
-}
-
-
